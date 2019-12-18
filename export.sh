a07ad98de3f54b76b2c9222354250f5826de4e87
Nout Kleef <noutkleef1@gmail.com>
2019-12-18 20:52:29 +0100

    adds VSCode configuration export

--- export.sh
+++ export.sh
@@ -5,6 +5,10 @@ rm -rf ~/.vim
 rm ~/.ssh/config
 rm ~/.vimrc
 rm ~/.bashrc
+rm ~/.config/Code/User/settings.json
+
+cp .bashrc ~/.bashrc
+echo "set up bash environment."
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cp -r .vim ~
@@ -14,7 +18,7 @@ echo "set up vim environment."
 cp .ssh/config ~/.ssh/config
 echo "set up ssh user configuration."
 
-cp .bashrc ~/.bashrc
-echo "set up bash environment."
+cp .config/Code/User/settings.json ~/.config/Code/User/settings.json
+echo "set up VSCode environment."
 
 echo "done!"
